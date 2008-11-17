1770fbd91f27dcbb75ca0e3cedb7a1230dbdf125
Hannes Schulz <temporaer@gmx.de>
2008-11-17 18:08:13 +0100

    use leading eigenvector, not eigenvalues

--- src/seriation_gen/lev_seriation.cpp
+++ src/seriation_gen/lev_seriation.cpp
@@ -1,6 +1,7 @@
 // 2008-11-17 Hannes Schulz <mail at hannes-schulz dot de>
 
 #include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/matrix_proxy.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/bindings/lapack/lapack.hpp>
 #include <factory/factory.h>
@@ -41,10 +42,16 @@ Serialization LEVSeriationGen::Impl::operator()(const ProbAdjPerm& pap)
 	L("Determine eigenvalues of laplacian\n");
 	ublas::matrix<double,ublas::column_major> Eigv(L);
 	ublas::vector<double> lambda(n);
+	ublas::vector<double>::iterator max_lambda;
+	int max_lambda_idx=0;
+
 	lapack::syev( 'V', 'L', Eigv, lambda, lapack::minimal_workspace() );
+	max_lambda = max_element(lambda.begin(),lambda.end());
+	max_lambda_idx = std::distance(lambda.begin(),max_lambda);
+	ublas::vector<double> leading = ublas::column(Eigv,max_lambda_idx);
 
 	L("eigenvalue-readout\n");
-	return readout_plain(lambda,*pap.getAdjMat());
+	return readout_plain(leading,*pap.getAdjMat());
 }
 
 #   define BEST_ELEM(X) max_element(X.begin(),X.end())
