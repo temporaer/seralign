5baf893272aeedf34a86e3f1215c1e9262435bbd
Hannes Schulz <temporaer@gmx.de>
2008-11-23 11:21:31 +0100

    disambiguate output param

--- src/util/mutagen.sh
+++ src/util/mutagen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for i in ../../data/mutagenesis/common/s*.csv; do
-	./hrmain --mutagenesis.in-file $i --out `basename $i .csv`.xml
+	./hrmain --mutagenesis.in-file $i --output `basename $i .csv`.txt 
 done
