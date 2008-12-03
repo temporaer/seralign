1c5b97521e1f933e7020b998966965562c11cc12
Hannes Schulz <temporaer@gmx.de>
2008-12-03 12:16:07 +0100

    call mutagen serialization with mutagen config file

--- src/util/mutagen-csv-to-xml.sh
+++ src/util/mutagen-csv-to-xml.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for i in ../../data/mutagenesis/common/s*.csv; do
-	./hrmain --mutagenesis.in-file $i --output `basename $i .csv`.xml
+	./hrmain -c mutagen.dat --mutagenesis.in-file $i --output `basename $i .csv`.xml
 done
