db52733380ab68b0b5bcefc7b5b567f65b65593f
Hannes Schulz <temporaer@gmx.de>
2008-12-06 00:48:33 +0100

    added origidx test

---
+++ src/tests/t_origidx.cpp
@@ -0,0 +1,56 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE origidx
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+#include <configuration.hpp>
+#include <jumbled_adjmat_gen.hpp>
+#include <boost/program_options.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <DegreeSort.hpp>
+#include <matlab_io.hpp>
+using namespace boost::program_options;
+namespace ublas = boost::numeric::ublas;
+using boost::any_cast;
+
+using namespace std;
+#define N 8
+
+struct Fixture{
+	AdjMat::AdjMatT adjmat; 
+	ProbAdjPerm pap;
+	
+	Fixture()
+		:adjmat(N,N)
+	{
+		for(int i=0;i<N;i++)
+			for(int j=i;j<N;j++){
+				switch(i){
+					case 0:  adjmat(i,j) = adjmat(j,i) = 3; break;
+					case 1:  adjmat(i,j) = adjmat(j,i) = 2; break;
+					case 3:  adjmat(i,j) = adjmat(j,i) = 1; break;
+					default: adjmat(i,j) = adjmat(j,i) = 0;
+				}
+			}
+		boost::shared_ptr<AdjMat::AdjMatT> ptr(new AdjMat::AdjMatT(adjmat));
+		pap.setAdjMat(ptr);
+	}
+	~Fixture(){
+	}
+};
+
+BOOST_FIXTURE_TEST_SUITE( suite, Fixture )
+
+BOOST_AUTO_TEST_CASE( testOrigIdx )
+{
+	matlab_matrix_out(cout,"sortedmat",*pap.getAdjMat());
+	DegreeSort ds;
+	ds.sort(pap);
+	matlab_matrix_out(cout,"sortedmat",*pap.getAdjMat());
+	BOOST_CHECK_EQUAL(pap.getOriginalIndex(N-1), 0);
+	BOOST_CHECK_EQUAL(pap.getOriginalIndex(N-2), 1);
+	BOOST_CHECK_EQUAL(pap.getOriginalIndex(N-3), 3);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
