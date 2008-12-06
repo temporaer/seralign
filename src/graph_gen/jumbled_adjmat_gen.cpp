88be14de22c91bb0eea4bb7fe52c02b606be9a03
Hannes Schulz <temporaer@gmx.de>
2008-12-06 16:52:17 +0100

    fix jumbledadjmatgen: permutation matrix was probably incorrect

--- src/graph_gen/jumbled_adjmat_gen.cpp
+++ src/graph_gen/jumbled_adjmat_gen.cpp
@@ -2,6 +2,7 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include "jumbled_adjmat_gen.hpp"
 #include <factory/factory.h>
+#include <nana.h>
 using namespace std;
 using namespace boost::numeric::ublas;
 
@@ -12,30 +13,43 @@ JumbledAdjMatGen::JumbledAdjMatGen(ProbAdjPerm& pap)
 
 ProbAdjPerm JumbledAdjMatGen::operator()()
 {
+	ProbAdjPerm pap;
 	int n = mPap.getAdjMat()->size1();
-	PermMat::PermMatT perm(zero_matrix<int>(n,n));
+	boost::shared_ptr<PermMat::PermMatT> perm(new PermMat::PermMatT(zero_matrix<int>(n,n)));
 	std::vector<int> order(n,0);
 	for(int i=0;i<n;i++)
 		order[i] = i;
 	random_shuffle(order.begin(),order.end());
 	for(int i=0;i<n;i++)
-		perm(i, order[i]) = 1;
+		(*perm)(i, order[i]) = 1;
 	matrix<double> tmp(n,n);
-	noalias(tmp) = prod(perm, *mPap.getAdjMat());
-	(*mPap.getAdjMat())  = prod(tmp, trans(perm));
+	noalias(tmp) = prod(*perm, *mPap.getAdjMat());
+	pap.setAdjMat( boost::shared_ptr<AdjMat::AdjMatT>(new AdjMat::AdjMatT(prod(tmp, trans(*perm)))));
 	
 	// save perm mat in problem
 	if(mPap.getPermMat().get() == NULL){
-		perm = trans(perm);
-		mPap.setPermMat(boost::shared_ptr<PermMat::PermMatT>(new PermMat::PermMatT(perm)));
+		*perm = trans(*perm);
+		pap.setPermMat(perm);
 	}
 	else{
-		*mPap.getPermMat() = prod(trans(perm),*mPap.getPermMat());
+		//*perm = prod(trans(*perm),*mPap.getPermMat());
+		*perm = prod(*mPap.getPermMat(), trans(*perm));
+		pap.setPermMat(perm);
 	}
 
-	noalias(tmp) = prod(perm, *mPap.getPermMat());
-	(*mPap.getPermMat()) = prod(tmp, trans(perm));
-	return mPap;
+//#ifndef NDEBUG
+	//tmp = prod(*pap.getPermMat(), *pap.getAdjMat());
+	//tmp = prod(tmp, trans(*pap.getPermMat()));
+	//matrix<double> tmp2(n,n);
+	//tmp2 = prod(*mPap.getPermMat(), *mPap.getAdjMat());
+	//tmp2 = prod(tmp2, trans(*mPap.getPermMat()));
+	//for(int i=0;i<n;i++)
+		//for(int j=0;j<n;j++){
+			//I(tmp(i,j) - tmp2(i,j) < 0.001);
+		//}
+//#endif
+
+	return pap;
 }
 JumbledAdjMatGen::~JumbledAdjMatGen()
 {
