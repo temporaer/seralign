a9b9df9f5ea7148b7fa510fc34ce51b0f26a2f5c
Hannes Schulz <temporaer@gmx.de>
2008-12-09 13:39:17 +0100

    mutagenesis-csv-to-xml: be more precise w.r.t. required output format

--- src/util/mutagen-csv-to-xml.sh
+++ src/util/mutagen-csv-to-xml.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#for i in ../../data/mutagenesis/common/s*.csv; do
+	#./hrmain -c mutagen.dat -f PlainPrint --mutagenesis.in-file $i --output `basename $i .csv`.dat
+#done
 for i in ../../data/mutagenesis/common/s*.csv; do
-	./hrmain -c mutagen.dat --mutagenesis.in-file $i --output `basename $i .csv`.xml
+	./hrmain -c mutagen.dat -f RealXMLPrint --mutagenesis.in-file $i --output `basename $i .csv`.xml
 done
+
+cp mutagen.dat ../../data/mutagenesis/results
