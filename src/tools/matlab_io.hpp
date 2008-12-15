c43b4f8ea607d3c87081635817316df42a729a79
Hannes Schulz <temporaer@gmx.de>
2008-12-15 18:58:16 +0100

    add csv output

--- src/tools/matlab_io.hpp
+++ src/tools/matlab_io.hpp
@@ -7,6 +7,30 @@
 #include	<string>
 
 template<class E, class T, class ME>
+inline void csv_matrix_out(std::basic_ostream<E, T> &os,ME& m){
+        typedef typename ME::size_type size_type;
+        size_type size1 = m.size1 ();
+        size_type size2 = m.size2 ();
+        std::basic_ostringstream<E, T, std::allocator<E> > s;
+        s.flags (os.flags ());
+        s.imbue (os.getloc ());
+        s.precision (os.precision ());
+        if (size1 > 0) {
+            if (size2 > 0)
+                s << "tmp,"<<m(0, 0);
+            for (size_type j = 1; j < size2; ++ j)
+                s << ',' << m(0, j);
+        }
+        for (size_type i = 1; i < size1; ++ i) {
+            s << "" << std::endl ;
+            if (size2 > 0)
+                s <<"tmp,"<< m(i, 0);
+            for (size_type j = 1; j < size2; ++ j)
+                s << ',' << m(i, j);
+        }
+        os << s.str().c_str ();
+}
+template<class E, class T, class ME>
 inline void matlab_matrix_out(std::basic_ostream<E, T> &os,const char* name,ME& m){
         typedef typename ME::size_type size_type;
         size_type size1 = m.size1 ();
