c76a2e5c54bea7058bd1c023f976b767d07b76a7
Hannes Schulz <temporaer@gmx.de>
2008-12-29 10:56:47 +0100

    icp: more test coverage. seems to work for small disturbances with rotation/translation in any order.

--- src/tests/t_icp.cpp
+++ src/tests/t_icp.cpp
@@ -32,37 +32,12 @@ struct F{
 
 BOOST_FIXTURE_TEST_SUITE( suite, Fixture )
 
-BOOST_AUTO_TEST_CASE( testTrans )
+BOOST_AUTO_TEST_CASE( testAll )
 {
 	ICP<DIM,Point,F> icp;
-	vector<Point> v, w;
-	const int S=9;
-	for(int i=0;i<S;i++){
-		Point p(DIM), q(DIM);
-		p[0] = i; p[1] = i*i; p[2] = 3*i;
-		v.push_back(p);
-		w.push_back(p);
-	}
-	for(int i=0;i<S; i++){
-		v[i] += ublas::scalar_vector<double>(3,0.1);
-		//swap(v[i][1],v[i][2]);
-		v[i][0] += 0.01*drand48();
-		v[i][1] += 0.01*drand48();
-		v[i][2] += 0.01*drand48();
-	}
-	icp.registerModel(v.begin(), v.end());
-	icp.match(w.begin(),w.end());
-	ICP<DIM,Point,F>::TQuat q = icp.getRot();
-	BOOST_CHECK_CLOSE(q.R_component_1(),1.0f,1.0);
-	BOOST_CHECK_CLOSE(q.R_component_2(),0.0f,1.0);
-	BOOST_CHECK_CLOSE(q.R_component_3(),0.0f,1.0);
-	BOOST_CHECK_CLOSE(q.R_component_4(),0.0f,1.0);
-}
-BOOST_AUTO_TEST_CASE( testRot )
-{
-	ICP<DIM,Point,F> icp;
+	icp.setVerbose(true);
 	vector<Point> model, query_orig, query;
-	const int S=10;
+	const int S=100;
 	for(int i=0;i<S;i++){
 		Point p(DIM), q(DIM);
 		p[0] = i; p[1] = 2*i; p[2] = 3*i;
@@ -76,6 +51,7 @@ BOOST_AUTO_TEST_CASE( testRot )
 		model[i] += ublas::scalar_vector<double>(3,0.1);
 		swap(model[i][1],model[i][2]);
 	}
+	model[S-1] += ublas::scalar_vector<double>(DIM,10);
 
 	// register model and match query
 	icp.registerModel(model.begin(), model.end());
@@ -88,21 +64,70 @@ BOOST_AUTO_TEST_CASE( testRot )
 	// test transformation properties
 	ICP<DIM,Point,F>::TQuat q = icp.getRot();
 	ICP<DIM,Point,F>::TVec  v = icp.getTrans();
-	cout << V(v)<<endl;
-	for(int i=0; i<S; i++){
+	for(int i=0; i<S-1; i++){ // ignore last=outlier
 		Point x = query_orig[i];
 		Point y = model[i];
 		ICP<DIM,Point,F>::TQuat qx(0,x[0],x[1],x[2]);
 		ICP<DIM,Point,F>::TQuat qy(0,y[0],y[1],y[2]);
 		ICP<DIM,Point,F>::TQuat qv(0,v[0],v[1],v[2]);
 		ICP<DIM,Point,F>::TQuat qxtrans = q * qx * boost::math::conj(q) + qv;
-		BOOST_CHECK_CLOSE(qxtrans.R_component_2(),qy.R_component_2(),1.0);
-		BOOST_CHECK_CLOSE(qxtrans.R_component_3(),qy.R_component_3(),1.0);
-		BOOST_CHECK_CLOSE(qxtrans.R_component_4(),qy.R_component_4(),1.0);
+		BOOST_CHECK_CLOSE(qxtrans.R_component_2(),qy.R_component_2(),5.0);
+		BOOST_CHECK_CLOSE(qxtrans.R_component_3(),qy.R_component_3(),5.0);
+		BOOST_CHECK_CLOSE(qxtrans.R_component_4(),qy.R_component_4(),5.0);
 	}
 
 }
 
+BOOST_AUTO_TEST_CASE( test2 )
+{
+	ICP<DIM,Point,F> icp;
+	icp.setVerbose(true);
+	vector<Point> model, query_orig, query;
+	const int S=100;
+	for(int i=0;i<S;i++){
+		Point p(DIM), q(DIM);
+		p[0] = i; p[1] = 2*i; p[2] = 3*i;
+		model.push_back(p);
+	}
+	copy(model.begin(),model.end(),back_inserter(query_orig));
+	copy(model.begin(),model.end(),back_inserter(query));
+
+	// modify model a bit
+	ICP<DIM,Point,F>::VectorRotator    rotate;
+	ICP<DIM,Point,F>::VectorTranslator translate;
+	ICP<DIM,Point,F>::TQuat qr(1,0.05,0.00,0.00);
+	qr /= boost::math::norm(qr);
+
+	for(int i=0;i<S; i++) {
+		model[i] = rotate(model[i], qr);
+		model[i] = translate(model[i], ublas::scalar_vector<float>(3,0.3));
+		//model[i] = rotate(model[i], qr);
+	}
+
+	// register model and match query
+	icp.registerModel(model.begin(), model.end());
+	icp.match(query.begin(),query.end());
+
+	cout << "trans: " <<icp.getTrans() <<endl;
+	cout << "rotmat:" << quaternion_to_R3_rotation(icp.getRot()) <<endl;
+	cout << "rot:   " << icp.getRot() <<endl;
+
+	// test transformation properties
+	ICP<DIM,Point,F>::TQuat q = icp.getRot();
+	ICP<DIM,Point,F>::TVec  v = icp.getTrans();
+	for(int i=0; i<S-1; i++){ // ignore last=outlier
+		Point x = query_orig[i];
+		Point y = model[i];
+		ICP<DIM,Point,F>::TQuat qx(0,x[0],x[1],x[2]);
+		ICP<DIM,Point,F>::TQuat qy(0,y[0],y[1],y[2]);
+		ICP<DIM,Point,F>::TQuat qv(0,v[0],v[1],v[2]);
+		ICP<DIM,Point,F>::TQuat qxtrans = q * qx * boost::math::conj(q) + qv;
+		BOOST_CHECK_LT(fabs(qxtrans.R_component_2()-qy.R_component_2()),0.5);
+		BOOST_CHECK_LT(fabs(qxtrans.R_component_3()-qy.R_component_3()),0.5);
+		BOOST_CHECK_LT(fabs(qxtrans.R_component_4()-qy.R_component_4()),0.5);
+	}
+
+}
 
 
 
