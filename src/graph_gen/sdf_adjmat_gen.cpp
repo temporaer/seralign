1e238ff262c6772cd3bf837e79f0975e9ff87f96
Hannes Schulz <temporaer@gmx.de>
2009-01-30 10:20:39 +0100

    sdf-graph-gen implemented

--- src/graph_gen/sdf_adjmat_gen.cpp
+++ src/graph_gen/sdf_adjmat_gen.cpp
@@ -7,6 +7,11 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/regex.hpp>
 #include <boost/foreach.hpp>
+#include <boost/graph/graphviz.hpp>
+
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+
 #include <factory/factory.h>
 #include <matlab_io.hpp>
 #include "sdf_adjmat_gen.hpp"
@@ -16,6 +21,19 @@
 using namespace std;
 using namespace boost;
 namespace ublas = boost::numeric::ublas;
+namespace fs = boost::filesystem;
+
+bool nextLineMatches(istream& is, string re){
+	string s;
+	getline(is, s);
+	regex e(re);
+	bool b = regex_match(s, e);
+	if(!b){
+		cerr << "Warning: Line `"<<s<<"' does not match expected pattern `"<<re<<"'"<<endl;
+		return false;
+	}
+	return true;
+}
 
 void SDFAdjmatGen::configure()
 {
@@ -31,39 +49,61 @@ SDFAdjmatGen::SDFAdjmatGen()
 }
 bool SDFAdjmatGen::hasNext()
 {
-	return true;
+	return mOutputCounter < (int)mDescriptors.size();
 }
 SDFAdjmatGen::~SDFAdjmatGen()
 {
 }
-std::string SDFAdjmatGen::getGraphID(const string& ref)
+std::string SDFAdjmatGen::getGraphID(const boost::any& ref)
 {
-	stringstream str;
-	return str.str();
+	return any_cast<Descriptor*>(ref)->name;
 }
-std::string SDFAdjmatGen::getGraphVizNodeAttribs(int idx, const string& ref)
+std::string SDFAdjmatGen::getGraphVizNodeAttribs(int idx, const boost::any& ref)
 {
-	I(ref == "");
 	return string("");
 }
-std::string SDFAdjmatGen::getPlainDescription(int ser_idx, const Serialization& ser, const string& ref)
+std::string SDFAdjmatGen::getPlainDescription(int ser_idx, const Serialization& ser, const boost::any& ref)
 {
-	I(ref == "");
 	stringstream str;
 	return str.str();
 }
-int SDFAdjmatGen::getClassID(const string& ref)
+int SDFAdjmatGen::getClassID(const boost::any& ref)
 {
-	return 0;
+	return any_cast<Descriptor*>(ref)->classid;
 }
 
 ProbAdjPerm SDFAdjmatGen::operator()()
 {
+	if(mDescriptors.size()==0){
+		if(fs::exists("/tmp/graphs.ser")){
+			ifstream ifs("/tmp/graphs.ser");
+			archive::binary_iarchive ar(ifs);
+			ar >> (*this);
+		}else
+			readInputFiles();
+		mOutputCounter = 0;
+	}
 
+	Descriptor& desc  = mDescriptors[mOutputCounter++];
+	string&     gname = desc.name;
+	SDFGraph& graph   = desc.graph;
+	SDFVertexIterator vi, vi_end;
+	tie(vi, vi_end) = bgl::vertices(graph);
+	int n = std::distance(vi, vi_end);
+
+	shared_ptr<AdjMat::AdjMatT> adj(new AdjMat::AdjMatT(n, n));
+	*adj = ublas::scalar_matrix<double>(n,n,0);
+	SDFEdgeIterator ei, ei_end;
+	for(tie(ei,ei_end)=bgl::edges(graph);ei!=ei_end;ei++){
+			(*adj)(bgl::source(*ei, graph),bgl::target(*ei,graph)) = 
+			(*adj)(bgl::target(*ei, graph),bgl::source(*ei,graph)) = 
+			bgl::get(bgl::get(bgl::edge_weight,graph),*ei);
+	}
 
 	ProbAdjPerm pap;
-	//pap.setAdjMat(adj);
-	pap.setId(getGraphID(""));
+	pap.setAdjMat(adj);
+	pap.setId(gname);
+	pap.setBackground(&desc.graph);
 
 	return pap;
 }
@@ -73,7 +113,7 @@ void SDFAdjmatGen::setInputFiles(const std::string& inputfileline)
 	string line = boost::trim_copy(inputfileline);
 
 	vector<string> file_class_pairs;
-	boost::split( file_class_pairs, line, boost::is_any_of(";") );
+	boost::split( file_class_pairs, line, boost::is_any_of(",") );
 
 	BOOST_FOREACH(const string& pair, file_class_pairs){
 		vector<string> tmp;
@@ -92,44 +132,64 @@ void SDFAdjmatGen::setInputFiles(const std::string& inputfileline)
 
 void SDFAdjmatGen::readInputFiles()
 {
+	int graphCount = 0;
 	BOOST_FOREACH(const FileDescriptor& fd, mInputFiles){
+		L("Reading SDF input file `%s'...", fd.name.c_str());
 		ifstream is(fd.name.c_str());
-		readMolekule(is);
+		while(readMolekule(is, fd.classid, graphCount++));
+		L(". %d molecules read.\n", graphCount);
 	}
+	ofstream os("/tmp/graphs.ser");
+	archive::binary_oarchive ar(os);
+	ar << const_cast<const SDFAdjmatGen&>(*this);
 }
 
-void SDFAdjmatGen::readMolekule(std::istream& is)
+bool SDFAdjmatGen::readMolekule(std::istream& is, int klass, int graphCount)
 {
+	Descriptor desc;
+	desc.classid = klass;
+
 	string line, id;
-	getline(is,line); // contains filename
-	getline(is,line); // contains id of openbabel converter
-	getline(is,line); // is empty
-	getline(is,line); // space separated: numberAtoms, numberBonds, unknown stuff....
+	if(!nextLineMatches(is, "\\s*"))            return false;
+	if(!nextLineMatches(is, ".*OpenBabel.*"))   return false;
+	if(!nextLineMatches(is, "\\s*"))            return false;
+	if(!nextLineMatches(is, ".*V3000"))         return false;
+	if(!nextLineMatches(is, ".*BEGIN	CTAB")) return false;
+	getline(is, line); // M V30 COUNTS numberAtoms numberBonds 0 0 0
 	boost::trim(line);
 	vector<string> strvec;
 	boost::split( strvec, line, boost::is_any_of("	") );
-	if(!strvec.size()==11){
+	if(!strvec.size()==8){
 		cerr << "Warning: SDFAdjmatGen: InputFileFormat Error: input description line `"<<line<<"' has unexpected format."<<endl;
-		return;
+		return false;
 	}
+	if(!nextLineMatches(is, ".*BEGIN	ATOM")) return false;
 
 	// create a matrix with numberAtoms size
-	int numberAtoms = boost::lexical_cast<int>(strvec[0]);
-	int numberBonds = boost::lexical_cast<int>(strvec[1]);
+	int numberAtoms = boost::lexical_cast<int>(strvec[3]);
+	int numberBonds = boost::lexical_cast<int>(strvec[4]);
 	int n = (mFixedSize==0) ? numberAtoms : mFixedSize;
-	shared_ptr<AdjMat::AdjMatT> adj(new AdjMat::AdjMatT(n, n));
+
+	desc.graph      = SDFGraph(n);
+	SDFGraph& graph = desc.graph;
 
 	// read all atoms
-	for(int i=0;i<numberAtoms; i++){
+	SDFVertexIterator vi, vi_end;
+	int atomCnt=0;
+	for(tie(vi,vi_end) = bgl::vertices(graph); vi!=vi_end; ++vi, ++atomCnt){
+		if(atomCnt == numberAtoms) break;
 		getline(is, line);
 		boost::trim(line);
 		boost::split( strvec, line, boost::is_any_of("	") );
-		if(strvec.size() != 9){
-			cerr << "Warning: SDFAdjmatGen: InputFileFormat Error: atom description line `"<<line<<"' has unexpected format."<<endl;
-			return;
+		if(strvec.size() < 8){
+			cerr << "Warning: SDFAdjmatGen: InputFileFormat Error: atom "<<graphCount<<" description line `"<<line<<"' has unexpected format."<<endl;
+			return false;
 		}
 		string atomType = strvec[3]; // C or N or...
+		bgl::get(bgl::vertex_name_t(),graph)[*vi] = atomType;
 	}
+	if(!nextLineMatches(is,".*END	ATOM"))return false;
+	if(!nextLineMatches(is,".*BEGIN	BOND"))return false;
 	
 	// read all bonds
 	for(int i=0;i<numberBonds; i++){
@@ -138,22 +198,50 @@ void SDFAdjmatGen::readMolekule(std::istream& is)
 		boost::split( strvec, line, boost::is_any_of("	") );
 		if(strvec.size() != 6){
 			cerr << "Warning: SDFAdjmatGen: InputFileFormat Error: bond description line `"<<line<<"' has unexpected format."<<endl;
-			return;
+			return false;
 		}
-		int atomFrom = boost::lexical_cast<int>(strvec[0])-1;
-		int atomTo   = boost::lexical_cast<int>(strvec[1])-1;
-		int bondType = boost::lexical_cast<int>(strvec[2]);
+		int atomFrom = boost::lexical_cast<int>(strvec[4])-1;
+		int atomTo   = boost::lexical_cast<int>(strvec[5])-1;
+		int bondType = boost::lexical_cast<int>(strvec[3]);
+		if(atomFrom >= numberAtoms || atomTo >= numberAtoms){
+			cerr << "Warning: SDFAdjmatGen: InputFileFormat Error: bond description line `"<<line<<"' has unexpected format."<<endl;
+			return false;
+		}
+		bgl::add_edge(atomFrom, atomTo, graph);
+		bgl::get(bgl::get(bgl::edge_weight, graph),bgl::edge(atomFrom, atomTo, graph).first) = bondType;
 	}
+	if(!nextLineMatches(is,".*END	BOND"))return false;
+	if(!nextLineMatches(is,".*END	CTAB"))return false;
+	if(!nextLineMatches(is,".*END"))return false;
+	if(!nextLineMatches(is,"\\$\\$\\$\\$"))return false;
 
-	// read foot
-	getline(is, line);
-	static const boost::regex e("END");
-	if(!regex_match(line, e))
-	{
-		cerr << "Warning: SDFAdjmatGen: InputFileFormat Error: end-line `"<<line<<"' has unexpected format."<<endl;
-		return;
-	}
+	stringstream sname;
+	sname << "SDF_class"<<klass<<"_"<<graphCount;
+	string gname = sname.str();
+	desc.name = gname;
+	bgl::get_property(graph, bgl::graph_name) = gname;
+	bgl::get_property(graph, bgl::graph_classid) = klass;
 
+	// debug output
+	/*
+	stringstream filename;
+	filename << "../../data/hiv/results/"<<gname<<".dot";
+	ofstream os(filename.str().c_str());
+	if(!os.is_open())
+		throw runtime_error(string("could not open for writing: ")+sname.str());
+
+	std::map<std::string,std::string> graph_attr, vertex_attr, edge_attr;
+	vertex_attr["label"] = gname;
+
+	bgl::write_graphviz(os, graph, 
+			bgl::make_label_writer(bgl::get(bgl::vertex_name,graph)),
+			bgl::make_label_writer(bgl::get(bgl::edge_weight,graph)),
+			bgl::make_graph_attributes_writer(graph));
+			*/
+	
+	mDescriptors.push_back(desc);
+
+	return true;
 }
 
 
--- src/graph_gen/sdf_adjmat_gen.hpp
+++ src/graph_gen/sdf_adjmat_gen.hpp
@@ -1,15 +1,51 @@
 #ifndef __SDF_ADJMAT_HPP__
 #define __SDF_ADJMAT_HPP__
 
-#include <boost/shared_ptr.hpp>
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/vector.hpp>
-#include <adjmat_gen.hpp>
 #include <vector>
 #include <map>
 
+#include <boost/shared_ptr.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/adj_list_serialize.hpp>
+#include <boost/filesystem.hpp>
+
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/list.hpp>
+//#include <boost/serialization/hash_set.hpp>
+//#include <boost/serialization/hash_map.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/string.hpp>
+
+#include <adjmat_gen.hpp>
+namespace bgl = boost;
+
+
+namespace boost{
+	enum graph_classid_t {graph_classid=111};
+	BOOST_INSTALL_PROPERTY(graph, classid);
+};
+
 class SDFAdjmatGen: public AdjMatGen{
 	private:
+		typedef bgl::adjacency_list<
+			bgl::listS, // store out-edges of vertex in std::list
+			bgl::vecS, // store vertex-set in std::vector
+			bgl::undirectedS, // not directed
+			bgl::property<bgl::vertex_name_t, std::string>,  // vertex-properties
+			bgl::property<bgl::edge_weight_t, double>,       // edge-properties
+			bgl::property<bgl::graph_name_t, std::string,    // graph-properties
+			  bgl::property<bgl::graph_classid_t, int>
+			>
+		>    SDFGraph;
+		typedef bgl::property_map<SDFGraph, bgl::graph_name_t>::type SDFGraphNameMap;
+		typedef bgl::property_map<SDFGraph, bgl::graph_classid_t>::type SDFGraphClassIDMap;
+		typedef bgl::property_map<SDFGraph, bgl::vertex_name_t>::type SDFVertexNameMap;
+		typedef bgl::property_map<SDFGraph, bgl::edge_weight_t>::type SDFEdgeWeightMap;
+		typedef bgl::graph_traits<SDFGraph>::vertex_descriptor SDFVertex;
+		typedef bgl::graph_traits<SDFGraph>::vertex_iterator SDFVertexIterator;
+		typedef bgl::graph_traits<SDFGraph>::edge_iterator SDFEdgeIterator;
 		int          mFixedSize;
 	public:
 		virtual void configure();
@@ -17,12 +53,13 @@ class SDFAdjmatGen: public AdjMatGen{
 		virtual ProbAdjPerm operator()();
 		virtual ~SDFAdjmatGen();
 		virtual bool hasNext();
-		virtual std::string getPlainDescription(int ser_idx, const Serialization&, const std::string&);
-		virtual std::string getGraphID(const std::string&);
-		virtual std::string getGraphVizNodeAttribs(int idx, const std::string&); //< should start with a comma!
-		virtual int getClassID(const std::string&);          ///< which class the graph belongs to
+		virtual std::string getPlainDescription(int ser_idx, const Serialization&, const boost::any&);
+		virtual std::string getGraphID(const boost::any&);
+		virtual std::string getGraphVizNodeAttribs(int idx, const boost::any&); //< should start with a comma!
+		virtual int getClassID(const boost::any&);          ///< which class the graph belongs to
 		
 	private:
+		int mOutputCounter;
 
 		struct FileDescriptor{
 			std::string name;
@@ -30,15 +67,28 @@ class SDFAdjmatGen: public AdjMatGen{
 		};
 		std::vector<FileDescriptor> mInputFiles; 
 
+		friend class boost::serialization::access;
+		template <class Archive>
+		void serialize(Archive& ar, const unsigned int version){
+			ar & mDescriptors;
+		}
+
 		struct Descriptor{
-			boost::shared_ptr<AdjMat::AdjMatT> mA_ptr;
-			int                                mClassID;
+			SDFGraph          graph;
+			int               classid;
+			std::string       name;
+			template <class Archive>
+				void serialize(Archive& ar, const unsigned int version){
+					ar & graph;
+					ar & classid;
+					ar & name;
+				}
 		};
-		std::map<std::string, Descriptor> mDescriptors;
+		std::vector<Descriptor>           mDescriptors;
 
 		void setInputFiles(const std::string& s);
 		void readInputFiles();
-		void readMolekule(std::istream& is);
+		bool readMolekule(std::istream& is,int klass, int cnt);
 };
 
 #endif /* __SDF_ADJMAT_HPP__ */
